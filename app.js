ca4c55049ad1c788c3188df6b12e2c7e02414dc8
北邙 <w915504817@gmail.com>
2024-05-03 08:03:22 +0800

    Bulma

--- app.js
+++ app.js
@@ -88,3 +88,12 @@ const openDB = () => {
       }
     }
   });
+
+  // 检索所选文件名
+  const fileInput = document.querySelector("#file-js-example input[type=file]");
+  fileInput.onchange = () => {
+    if (fileInput.files.length > 0) {
+      const fileName = document.querySelector("#file-js-example .file-name");
+      fileName.textContent = fileInput.files[0].name;
+    }
+  };
